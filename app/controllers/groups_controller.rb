82dcb0794c96954dbf0737ee583f56d1bb6eb5af
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2017-01-17 01:53:46 +0900

    edited groups_controller and top_controller: convert <before_action :authenticate_user!, :only => [:index]> to <before_action :authenticate_user!, only: :index>

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, :only => [:index]
+  before_action :authenticate_user!, only: :index
   def new
   end
   def edit
