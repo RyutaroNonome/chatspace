f31038d127c203e8bf59c36205fef8d3dc5ac1ae
野々目龍太郎 <NonomeRyutaro@nonomeno-MacBook-Pro.local>
2017-01-18 18:31:26 +0900

    modified some files because of requests by Ina-sen

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -13,8 +13,8 @@ class GroupsController < ApplicationController
   def create
     @group = current_user.groups.new(group_params)
     if @group.save
-      # redirect_to group_messages_path(@group)
-      # グループ作成テスト
+      # redirect_to group_messages_path(@group) ← メッセージで使うはず
+      # グループ作成テスト ← のちほどパス変えるかも
       redirect_to new_group_path, notice: '新規グループを作成しました(｀・ω・´)ｂ'
     else
       flash[:alert] = '新規グループ作成に失敗しました(´・ω・｀)'
@@ -29,7 +29,7 @@ class GroupsController < ApplicationController
     if @group.update(group_params)
       # redirect_to group_messages_path(@group)
 
-      # グループテスト
+      # グループ編集テスト ← のちほどパス変えるかも
       redirect_to new_group_path, notice: 'グループを編集しました(｀・ω・´)ｂ'
     else
       flash[:alert] = 'グループ編集に失敗しました(´・ω・｀)'
@@ -38,6 +38,7 @@ class GroupsController < ApplicationController
   end
 
   private
+
   def set_group
     @group = Group.includes(:users).find(params[:id])
   end
